4cd7bec5def6a52b789a00404995c573f2f0b6ca
jumisz <juan.m.salamanca@gmail.com>
2010-12-26 11:44:40 +0000

    More changes

--- core/pictures.py
+++ core/pictures.py
@@ -1,17 +1,35 @@
-'''
-Created on 19 Dec 2010
+import os
+from sqlite3 import *
 
-@author: juan
-'''
-
-class Picture(object):
-    '''
-    classdocs
-    '''
-
-
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
+image_exts = ['jpg','png','gif','tif','bmp','xpm']
         
+def get_connection():
+    return connect('../db/picturelogic.db')
+
+def get_pictures_insert_stmt(picture):
+    return 'INSERT INTO PICTURES ( filename, path) VALUES (\'%(filename)s\',  \'%(path)s\')' % \
+                        picture
+
+def insert_picture(picture):
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute(get_pictures_insert_stmt(picture))
+    connection.commit()
+    cursor.close()
+
+
+def import_from_folder(folder):
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if is_image(file):
+                picture = {}
+                picture['filename'] = file
+                picture['path'] = os.path.join(root,file)
+                insert_picture(picture)
+    
+def is_image(file):
+    for ext in image_exts:
+        if ext in file: 
+            return True
+        
+    return False        
--- picturelogic.py
+++ picturelogic.py
@@ -1,14 +1,39 @@
-import sys
-try:  
-    import pygtk  
-    pygtk.require("2.0")  
-except:  
-    pass  
-try:  
-    import gtk  
-    import gtk.glade  
-except:  
-    print("GTK Not Availible")
-    sys.exit(1)
+import gtk
     
+class PictureLogic:
+    def close(self, widget):
+        gtk.main_quit()
+          
+    def __init__(self):
+          self.builder = gtk.Builder()
+          self.builder.add_from_file("gui/gui.glade")
+          self.window = self.builder.get_object("window1")
+          self.window.show()
+          dic = {
+                 "on_quit_menuitem_activate" : self.close,
+                 "on_about_menuitem_activate" : self.about,
+                 "on_import_folder_activate": self.importFolder,
+                 "on_open_image_activate": self.openImage
+                 }
+          self.builder.connect_signals(dic)
+          
+    def about(self,widget):
+        self.about = About(self.builder)
+        
+    def importFolder(self,widget):
+        self.importFolderDialog = self.builder.get_object("importfolderdialog")
+        self.importFolderDialog.show()
+    def openImage(self,widget):
+        self.openImageDialog = self.builder.get_object("openimagedialog")
+        self.openImageDialog.show()
+class About:
+    
+    def __init__(self, builder):
+        self.builder = builder
+        self.window = builder.get_object("aboutdialog")
+        self.window.show()
+            
+
+PictureLogic()
+gtk.main()   
     
