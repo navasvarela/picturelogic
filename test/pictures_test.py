da527bc8169c77ac8051bce4e4f21819c98c3be4
jumisz <juan.m.salamanca@gmail.com>
2010-12-26 11:43:15 +0000

    Added tests and more UI

---
+++ test/pictures_test.py
@@ -0,0 +1,21 @@
+import unittest
+from pictures import *
+
+
+
+class TestPictures(unittest.TestCase):
+    
+    
+    def test_get_pictures_insert_stmt(self):
+        picture = { 'filename':'picture1file','path':'/tmp/pictureone'}
+        insert_stmt = get_pictures_insert_stmt(picture)
+        print insert_stmt
+        for key in picture.keys():
+            print "testing key: "+key
+            self.assertTrue(picture[key] in insert_stmt)
+             
+    def test_import_from_folder(self):
+        import_from_folder('/home/juan/Pictures/Photos')
+        
+      
+
