e450a24d6190ea6f935cd3ea2ac5e558a5ac09ed
jumisz <juan.m.salamanca@gmail.com>
2011-01-10 14:57:03 +0000

    Started work on tags

--- core/pictures.py
+++ core/pictures.py
@@ -1,27 +1,45 @@
 import os
-import Image
 import logging
 import logging.config 
 from sqlite3 import *
 from config import * 
+import Image    
+from ExifTags import TAGS
+
 
 # create logger
 logging.config.fileConfig(LOGGING_CONF) #@UndefinedVariable
 logger = logging.getLogger("pictures")
   
 
-image_exts = ['jpg', 'JPG','png', 'gif', 'tif', 'bmp', 'xpm']
+image_exts = ['jpg', 'JPG', 'png', 'gif', 'tif', 'bmp', 'xpm']
         
 def get_connection():
     print "Connecting to DB: " + DB_FILE   #@UndefinedVariable
     return connect(DB_FILE)  #@UndefinedVariable
 
+def execute_sql(sql):
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    connection.commit()
+    cursor.close()
+
 def get_pictures_insert_stmt(picture):
     return 'INSERT INTO PICTURES ( filename, path, thumbnail) VALUES (\'%(filename)s\',  \'%(path)s\', \'%(thumbnail)s\')' % \
                         picture
 
+def get_tags_insert_stmt(tagname):
+    return 'INSERT OR IGNORE INTO TAGS (name) VALUES (\'%s\')' % tagname
+
+def get_picturetags_insert_stmt(pictureid, tagname):
+    return 'INSERT INTO PICTURETAGS (pictureid, tagid) VALUES ( %d, (SELECT ID FROM TAGS WHERE NAME = \'%s\'))' % (pictureid, tagname)
+
 def select_pictures_stmt():
     return 'SELECT * FROM PICTURES'
+
+def get_insert_exifs_stmt(exif, picture_path):
+    return 'INSERT INTO EXIFS (pictureid, date, exposure, size) VALUES((SELECT ID FROM PICTURES WHERE PATH = \'%s\'), '
 
 def insert_picture(picture):
     connection = get_connection()
@@ -30,6 +48,16 @@ def insert_picture(picture):
     connection.commit()
     cursor.close()
 
+def insert_tags(pictureids, tags):
+    connection = get_connection()
+    cursor = connection.cursor()
+    for tag in tags.split(','):
+        tag = tag.strip()
+        cursor.execute(get_tags_insert_stmt(tag))
+        for pictureid in pictureids:
+            cursor.execute(get_picturetags_insert_stmt(pictureid, tag))
+    connection.commit()
+    cursor.close()        
 
 def get_pictures_from_db():
     connection = get_connection()
@@ -47,8 +75,10 @@ def import_from_folder(folder):
                 picture['filename'] = file
                 picture['path'] = os.path.join(root, file)
                 picture['thumbnail'] = generate_and_save_thumbnail(picture['path'], 120, 160, ".jpg")
+                exif = get_exif(picture['path'])
+                print exif
                 insert_picture(picture)
-    print "Finished Importing pictures"
+    logger.debug("Finished Importing pictures")
     
 
     
@@ -60,7 +90,7 @@ def is_image(file):
     return False  
 
 def generate_and_save_thumbnail(imageFile, h, w, ext):
-    logger.debug("generating thumbnail for file "+imageFile)
+    logger.debug("generating thumbnail for file " + imageFile)
     image = Image.open(imageFile)
     image = image.resize((w, h), Image.ANTIALIAS)
     outFileLocation = "./thumbnails"
@@ -70,3 +100,16 @@ def generate_and_save_thumbnail(imageFile, h, w, ext):
         os.makedirs(dirname)        
     image.save(outFile)      
     return outFile
+
+def get_exif(imageFile):
+    logger.debug("extracting exif for file: " + imageFile)
+    ret = {}
+    i = Image.open(imageFile)
+    info = i._getexif()
+    if (info == None):
+        logger.debug("No exif information found for file: " + imageFile)
+        return
+    for tag, value in info.items():
+        decoded = TAGS.get(tag, tag)
+        ret[decoded] = value
+    return ret
--- picturelogic.py
+++ picturelogic.py
@@ -21,6 +21,7 @@ class PictureLogic:
           self.builder.add_from_file("gui/gui.glade")
           self.window = self.builder.get_object("window1")
           self.window.show()
+          self.addTagsDialog = self.builder.get_object("addtags_dialog")
           dic = {
                  "on_quit_menuitem_activate" : self.close,
                  "on_about_menuitem_activate" : self.about,
@@ -30,7 +31,9 @@ class PictureLogic:
                  "on_search_button_clicked" : self.search,
                  "on_pictures_iconview_item_activated" : self.iconview_item_activated,
                  "on_pictures_iconview_selection_changed" : self.iconview_selection_changed,
-                 "on_pictures_iconview_button_press_event": self.iconview_button_press_events
+                 "on_pictures_iconview_button_press_event": self.iconview_button_press_events,
+                 "on_addtags_cancel_button_clicked": self.addTagsDialog_destroy,
+                 "on_addtags_ok_button_clicked": self.doAddTags
                  }
           self.builder.connect_signals(dic)
           self.iconView = self.builder.get_object("pictures_iconview")
@@ -59,7 +62,7 @@ class PictureLogic:
         self.openImageDialog = self.builder.get_object("openimagedialog")
         self.openImageDialog.show()
     def refresh_images(self):
-        self.pictures_store = gtk.ListStore(gtk.gdk.Pixbuf, gobject.TYPE_STRING)
+        self.pictures_store = gtk.ListStore(gtk.gdk.Pixbuf, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_INT)
         self.iconView.set_model(self.pictures_store)
         self.iconView.set_pixbuf_column(0)
         self.iconView.set_text_column(1)
@@ -67,9 +70,8 @@ class PictureLogic:
         if self.pictures == []:
             return
         for picture in self.pictures:
-            print picture
             pixbuf = gtk.gdk.pixbuf_new_from_file(picture[0])
-            self.pictures_store.append([pixbuf,picture[2]])
+            self.pictures_store.append([pixbuf,picture[2], picture[0], picture[3]])
         
     def search(self,widget):
         pass
@@ -84,22 +86,44 @@ class PictureLogic:
         
         pass
         
-    def iconview_button_press_events(self,widget, event):
-        logger.debug("button_press_event, event: %d " % event.button)
+    def iconview_button_press_events(self,widget, event):      
         item = self.iconView.get_path_at_pos(event.x, event.y)
-        
-        if (event.button == 3 and item != None ):
-         
+        if (event.button == 3 and item != None ):  
           logger.debug("item where right click ocurred: "+self.pictures_store[item][1])
-        pass
+          self.iconView.select_path(item)
+          self.iconMenu = gtk.Menu()
+          addTagsItem = gtk.MenuItem("Add tags")
+          addTagsItem.connect("activate", self.addTags)
+          removeTagsItem = gtk.MenuItem("Remove tags")
+          self.iconMenu.append(addTagsItem)
+          self.iconMenu.append(removeTagsItem)
+          self.iconMenu.popup(None, None, None, event.button, event.time, None)
+          self.iconMenu.show_all()
+    def addTags(self, widget):
+        self.addTagsDialog.show()
+        
+    def doAddTags(self, widget):
+        logger.debug("Adding tags to pictures selected")
+        selectedItems = self.iconView.get_selected_items()
+        pictureids = []
+        for item in selectedItems:
+            pictureids.append(self.pictures_store[item][3])
+        addtagsEntry = self.builder.get_object("addtags_entry")
+        selectedTags = addtagsEntry.get_text()
+        if selectedTags != '':
+            insert_tags(pictureids, selectedTags)
+        self.addTagsDialog_destroy(self,widget)
+    def addTagsDialog_destroy(widget):
+        logger.debug("Destroying add tags dialog")
+        self.addTagsDialog.destroy()
 class About:
     
     def __init__(self, builder):
-        self.builder = builder
+        
         self.window = builder.get_object("aboutdialog")
         self.window.show()
             
-
+    
 PictureLogic()
 gtk.main()   
     
